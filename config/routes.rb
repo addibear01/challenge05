20cf084848b0432d81cddbf2099d549c9e215d6a
addiradical <ajanzen-winter2@rrc.ca>
2024-06-11 12:50:15 -0500

    Starting Pt2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'product/show'
-  get 'products/index'
+  get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   root to: 'products#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
